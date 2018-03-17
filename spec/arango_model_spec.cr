ce2ae64ceb07d5131ce990492ec736d6cf4a5e59
Matt Petty <matt@kizmeta.com>
2018-03-17 02:18:46 -0500

    change attributes

--- spec/arango_model_spec.cr
+++ spec/arango_model_spec.cr
@@ -1,13 +1,11 @@
 require "./spec_helper"
 
 class Ninja < ArangoModel::Base
-  # DEFAULT_JSON = {"t" => "x"}
   attribute name : String
   attribute epithet : String
   attribute age : Int32
-  attribute enemies : Array(String) = [] of String
-  attribute properties : JSON::Any
-  # attribute enemies : JSON::Any
+  attribute weapons : Array(String) = [] of String
+  attribute enemies : JSON::Any
 end
 
 describe ArangoModel do
@@ -15,26 +13,26 @@ describe ArangoModel do
     Ninja.new.should_not be_nil
   end
 
-  it "should have a string attribute" do
+  it "should support a string attribute" do
     ninja = Ninja.new name: "Hiro"
     ninja.name.should eq("Hiro")
   end
 
-  it "should have an integer attribute" do
+  it "should support an integer attribute" do
     ninja = Ninja.new age: 25
     ninja.age.should eq(25)
   end
 
-  it "should generally work okay" do
-    (ninja=Ninja.new).enemies = %w[ Joe Barry ]
-    (ninja.enemies || [] of String).first.should eq("Joe")
-    (ninja.enemies || [] of String).last.should eq("Barry")
+  it "should support an array attribute" do
+    (ninja=Ninja.new).weapons = %w[ katana shuriken ]
+    (ninja.weapons || [] of String).first.should eq("katana")
+    (ninja.weapons || [] of String).last.should eq("shuriken")
   end
 
-  it "should support json" do
+  it "should support a json attribute" do
     ninja = Ninja.new
-    ninja.properties = JSON::Any.new({"this" => "that"}.to_json)
-    ninja.properties.should eq("{\"this\":\"that\"}")
+    ninja.enemies = JSON::Any.new([{"name" => "Hiro"}, {"name" => "Shane"}].to_json)
+    ninja.enemies.should eq("[{\"name\":\"Hiro\"},{\"name\":\"Shane\"}]")
   end
 
   # it "should have an array attribute" do
